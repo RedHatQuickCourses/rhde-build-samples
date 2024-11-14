9d06cdbb0a4c79a9a1c29dfabf1735c5882ef6b3
Fernando Lozano <flozano@redhat.com>
2024-11-14 16:13:53 -0500

    script to download microshift containers

---
+++ sh/download-microshift.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PULL_SECRET_FILE=~/Downloads/pull-secret
+IMAGE_LIST_FILE=~/microshift-container-refs.txt
+IMAGE_LOCAL_DIR=~/microshift-containers
+
+while read -r src_img ; do
+   # Remove the source registry prefix
+   dst_img=$(echo "${src_img}" | cut -d '/' -f 2-)
+
+   # Run the image download command
+   echo "Downloading '${src_img}' to '${IMAGE_LOCAL_DIR}'"
+   mkdir -p "${IMAGE_LOCAL_DIR}/${dst_img}"
+   skopeo copy --all --quiet \
+      --preserve-digests \
+      --authfile "${PULL_SECRET_FILE}" \
+      docker://"${src_img}" dir://"${IMAGE_LOCAL_DIR}/${dst_img}"
+
+done < "${IMAGE_LIST_FILE}"
+
